5fc8d564336b52c46d4974abd4ad7cb1804e6b83
Frank Sossi <frank.sossi@gmail.com>
2022-04-27 15:51:50 -0700

    Configured update function in ship.py to get speed from settings.py and limited movement to the righ and left side of the screen

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game) -> None:
         """Set up ship """
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -14,16 +15,22 @@ class Ship:
         # start each ship at bottom center
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store decimal value for ships horizontal position
+        self.x = float(self.rect.x)
+
         # Movement Flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """Update the ships positions"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
