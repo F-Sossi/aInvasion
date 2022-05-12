096b6d53c074d8265624c39dc20b82186f6ddb03
Frank Sossi <frank.sossi@gmail.com>
2022-05-12 14:07:26 -0700

    updated alien_invasion.py refactored _create_fleet adding _create_alien function and modified _create_fleet to add full fleet based on screen size opchecked good

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,18 +34,33 @@ class AlienInvasion:
         # Create alien and find the number of aliens in a row
         # Spacing is one alien width
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
         alien_width = alien.rect.width
         available_space_x = self.settings.screen_width - (2*alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Create row of aliens 
-        for alien_number in range(number_aliens_x):
-            # create alien
-            alien = Alien(self)
-            # set alien x value (position) one alien width over from last alien created
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # determine the number of rows that will fit on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                    (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+
+        # Create full fleet of aliens 
+        for row_number in range(number_rows): 
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+            
+            
+    def _create_alien(self, alien_number, row_number):        
+        # create alien
+        alien = Alien(self)
+        alien_height, alien_width = alien.rect.size 
+        # set alien x value (position) one alien width over from last alien created
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def _update_bullets(self):
         """Update the position of bullets and clear old bullets"""
